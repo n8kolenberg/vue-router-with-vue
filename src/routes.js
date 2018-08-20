61f0f4b19ea5f1bb7b981c9886bc59e77080134a
Nathaniel Kolenberg <nathanielkolenberg@Nathaniels-MacBook-Pro.local>
2018-08-20 19:58:56 +0400

    Use lazy loading

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,43 @@
 /**Routes.js is used in main.js and set up with VueRouter to be used for routing users within the app */
 
-
-import User from './components/user/User.vue'
+/** Everything loaded through imports at the top will always be loaded in your bundle whether you use them or not*/
+// import User from './components/user/User.vue'
 import Home from './components/Home.vue'
-import UserStart from './components/user/UserStart.vue'
-import UserDetail from './components/user/UserDetail.vue'
-import UserEdit from './components/user/UserEdit.vue'
+// import UserStart from './components/user/UserStart.vue'
+// import UserDetail from './components/user/UserDetail.vue'
+// import UserEdit from './components/user/UserEdit.vue'
 import Header from './components/Header.vue'
+
+//The following syntax is to utilise lazy loading
+//I.e. only loading the packages that are needed by the user WHEN they are needed
+//Be prepared for some mindfu^&ery in terms of syntax
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User.vue'));
+    });
+}
+
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart.vue'));
+    });
+}
+
+
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail.vue'));
+    });
+}
+
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit.vue'));
+    });
+}
+
+
+
 
 export const routes = [
     
